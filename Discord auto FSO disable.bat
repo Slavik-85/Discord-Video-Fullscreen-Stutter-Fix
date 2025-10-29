d9b9a49dd4f206393d4f66d2b8c619c56386378b
Slavik-85 <79378317+Slavik-85@users.noreply.github.com>
2025-10-29 17:12:22 +0200

    Add admin check to Discord FSO disable script

--- Discord auto FSO disable.bat
+++ Discord auto FSO disable.bat
@@ -1,4 +1,10 @@
 @echo off
+:: Check admin (net session fails if not admin)
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
 
 rem --- Variables ---
 set "ps1file=%~dp0discordfsooff.ps1"
